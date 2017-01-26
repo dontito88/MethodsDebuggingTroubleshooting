92984394d2bd535ac366eed5a2c477554a4c75df
TrifonEstov <don.tito.g@gmail.com>
2017-01-26 23:27:52 +0200

    Another problem solved!

---
+++ SignOfInteger/SignOfInteger.cs
@@ -0,0 +1,29 @@
+﻿namespace SignOfInteger
+{
+    using System;
+
+    public class Methods
+    {
+        public static void SignOfNumber(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+        }
+        static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            SignOfNumber(number);
+        }
+
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class SignOfInteger
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        PrintSign(number);
+    }
+
+    private static void PrintSign(int number)
+    {
+        if (number > 0)
+        {
+            Console.WriteLine($"The number {number} is positive.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine($"The number {number} is negative.");
+        }
+        else
+        {
+            Console.WriteLine("The number 0 is zero.");
+        }
+    }
+}
