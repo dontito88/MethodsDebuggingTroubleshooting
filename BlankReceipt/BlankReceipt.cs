e8849d4b9aecf3ec129a361beccac53f07876449
TrifonEstov <don.tito.g@gmail.com>
2017-01-26 19:21:02 +0200

    List updated

---
+++ BlankReceipt/BlankReceipt.cs
@@ -0,0 +1,22 @@
+﻿namespace BlankReceipt
+{
+    using System;
+
+    public class Methods
+    {
+        static void Main()
+        {
+            BlanckReceipt();
+        }
+        public static void BlanckReceipt()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\u00A9 SoftUni");
+        }
+        
+    }
+}
