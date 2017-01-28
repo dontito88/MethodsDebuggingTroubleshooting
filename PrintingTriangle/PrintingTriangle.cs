a0825ce7e8600b49262461d03e7254ffeb048a61
TrifonEstov <don.tito.g@gmail.com>
2017-01-28 17:46:07 +0200

    New Problem solved!

---
+++ PrintingTriangle/PrintingTriangle.cs
@@ -0,0 +1,38 @@
+﻿namespace BlankReceipt
+{
+    using System;
+
+    public class Methods
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            PrintTriangleTop(n);
+            PrintTriangleBottom(n);
+        }
+        public static void PrintTriangleTop(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write($"{col} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintTriangleBottom(int n)
+        {
+            for (int row = n - 1; row >= 1; row--)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write($"{col} ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
