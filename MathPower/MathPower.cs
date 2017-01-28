de44365002df02d0a18d33592dfef87fe5978454
TrifonEstov <don.tito.g@gmail.com>
2017-01-28 18:40:26 +0200

    Program added.

---
+++ MathPower/MathPower.cs
@@ -0,0 +1,23 @@
+﻿namespace MathPower
+{
+   using System;
+
+   public class MathPower
+    {
+        public static void Main()
+        {
+            var baseNumber = double.Parse(Console.ReadLine());
+            var power = int.Parse(Console.ReadLine());
+            Console.WriteLine(RaiseToPower(baseNumber, power));
+        }
+        private static double RaiseToPower(double baseNumber, int power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= baseNumber;
+            }
+            return result;
+        }
+    }
+}
