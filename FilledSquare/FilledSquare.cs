d7233272934bb2cfdebb58e103e638696632ba2f
TrifonEstov <don.tito.g@gmail.com>
2017-01-28 18:08:13 +0200

    The solution of FilledSquare problem is incorrect!!!

---
+++ FilledSquare/FilledSquare.cs
@@ -0,0 +1,31 @@
+﻿namespace FilledSquare
+{
+    using System;
+
+    public class Methods
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            PrintTopAndBotton(n);
+            PrintMiddle(n);
+            PrintTopAndBotton(n);
+        }
+        public static void PrintTopAndBotton(int n)
+        {
+            Console.WriteLine(new string('-', n * 2));
+        }
+        public static void PrintMiddle(int n)
+        {
+            for (int i = 1; i <= n / 2; i++)
+            {
+                Console.Write('-');
+                for (int j = 1; j <= n - 1; j++)
+                {
+                    Console.Write("\\/");
+                }
+                Console.WriteLine('-');
+            }
+        }
+    }
+}
