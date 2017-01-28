8e5da12fc8f0ebf700a374c3795056d3ed048833
TrifonEstov <don.tito.g@gmail.com>
2017-01-28 18:27:56 +0200

    New program Added!

---
+++ TriangleArea/TriangleArea.cs
@@ -0,0 +1,20 @@
+﻿namespace TriangleArea
+{
+    using System;
+
+    public class TriangleArea
+    {
+        public static void Main()
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            double area = AreaCalc(a, b);
+            Console.WriteLine(area);
+
+        }
+        private static double AreaCalc(double a, double b)
+        {
+            return a * b / 2;
+        }
+    }
+}
